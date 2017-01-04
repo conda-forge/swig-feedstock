477f21940943647553df1d1aa2cc4336149fb292
Julien Schueller <schueller@phimeca.com>
2017-01-04 23:01:15 +0100

    Use --without-alllang

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,27 +6,7 @@
              --with-boost="${PREFIX}" \
              --with-tcl="${PREFIX}" \
              --with-tclconfig="${PREFIX}/lib" \
-             --without-perl5 \
-             --without-octave \
-             --without-scilab \
-             --without-java \
-             --without-javascript \
-             --without-gcj \
-             --without-android \
-             --without-guile \
-             --without-mzscheme \
-             --without-ruby \
-             --without-php \
-             --without-ocaml \
-             --without-pike \
-             --without-chicken \
-             --without-csharp \
-             --without-lua \
-             --without-allegrocl \
-             --without-clisp \
-             --without-r \
-             --without-go \
-             --without-d
+             --without-alllang
 make -j${CPU_COUNT}
 #make check
 make install
