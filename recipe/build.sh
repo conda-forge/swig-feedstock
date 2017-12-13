b4345a2fc3619b8927048c7e50bbdac8b1200844
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-12-12 22:35:04 -0600

    Specify host to configure script

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,10 @@ export CXX=$(basename ${CXX})
 
 "${SRC_DIR}"/configure \
              --prefix="${PREFIX}" \
+             --host="${HOST}" \
              --with-pcre-prefix="${PREFIX}" \
              --without-alllang
+
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
