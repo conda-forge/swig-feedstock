d93466549fa83481161182a5979fc31ce9ea4d1d
Julien Schueller <schueller@phimeca.com>
2019-06-28 17:42:23 +0200

    Cleanup build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,6 @@
 ./configure \
              --prefix="${PREFIX}" \
              --with-pcre-prefix="${PREFIX}" \
-             --with-boost="${PREFIX}" \
-             --with-tcl="${PREFIX}" \
-             --with-tclconfig="${PREFIX}/lib" \
              --without-alllang
 make -j${CPU_COUNT}
-#make check
 make install
