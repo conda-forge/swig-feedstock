636779124e0a90b901680ed58312699c9e2596fe
Julien Schueller <schueller@phimeca.com>
2020-06-09 09:01:52 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,6 @@
 ./configure \
              --prefix="${PREFIX}" \
              --with-pcre-prefix="${PREFIX}" \
-             --with-boost="${PREFIX}" \
              --without-alllang
 make -j${CPU_COUNT}
 #make check
