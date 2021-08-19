c77f25ed9ae6468fdff8d0a697f05a1ce9be4f35
Kai Tietz <ktietz@anaconda.com>
2021-08-19 10:04:47 +0200

    add support for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./Tools/config
+
 # Full paths break make check
 export CC=$(basename ${CC})
 export CXX=$(basename ${CXX})
