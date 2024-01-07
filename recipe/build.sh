97a54e494d370a955a30672a104a470a89a006d7
Julien Schueller <schueller@phimeca.com>
2024-01-07 15:11:41 +0100

    Add swig4.0 symlink
    Closes #47

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,3 +8,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./Tools/config
              --without-alllang
 make -j${CPU_COUNT}
 make install
+
+# cmake prefers the versioned symlink:
+# https://github.com/Kitware/CMake/blob/v3.28.1/Modules/FindSWIG.cmake#L89
+ln -sf ${PREFIX}/bin/swig ${PREFIX}/bin/swig4.0
